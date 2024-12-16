af5a46435dc8bc294975cced5457c5e62818c4c3
Martin Rådmans <martin.radmans@iths.se>
2024-12-16 13:25:11 +0100

    Started up the project. Going with Next js with typescript. Implemented material UI for the styling. Added all the folders and page.tsx for the project. Gonna start with contentful to create whats needed than start the coding to implement the content

---
+++ cms-project/src/app/404/404.tsx
@@ -0,0 +1,3 @@
+export default function errorHandler() {
+  return <h1>404</h1>;
+}
---
+++ cms-project/src/app/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2", // Din primärfärg
+    },
+    secondary: {
+      main: "#dc004e", // Din sekundärfärg
+    },
+  },
+});
+
+export default theme;
