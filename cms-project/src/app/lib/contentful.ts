f925459ec200f71e1f630b742d78a5af47e94d83
Martin Rådmans <martin.radmans@iths.se>
2024-12-17 12:53:51 +0100

    Been sitting on contentful to create the content needed.

--- cms-project/src/app/lib/contentful.ts
+++ cms-project/src/app/lib/contentful.ts
@@ -1,6 +1,11 @@
 import { createClient } from "contentful";
 
 export const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID!,
-  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
+  space: process.env.CONTENTFUL_SPACE_ID || "",
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "",
 });
+
+export const fetchEntries = async (contentType: string) => {
+  const entries = await client.getEntries({ content_type: contentType });
+  return entries.items;
+};
