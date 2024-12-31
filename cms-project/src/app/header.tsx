e732be05dd3505832ab9b350989f8ab479edb2f7
Martin Rådmans <martin.radmans@iths.se>
2024-12-31 11:26:46 +0100

    Added a searchbar to the navmenu. Will add later a way to search for content on the site. But for now leave it

--- cms-project/src/app/header.tsx
+++ cms-project/src/app/header.tsx
@@ -8,9 +8,16 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Link from "next/link";
+import InputBase from "@mui/material/InputBase";
+import { Paper, useMediaQuery, useTheme } from "@mui/material";
+import Divider from "@mui/material/Divider";
 
 export default function Header() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  // Hook to check if the user on smaller screens.
+  // Remove the searchbar if user screen is smaller than the sm breakpoint
 
   const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
@@ -27,6 +34,23 @@ export default function Header() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             <h1>Headless CMS Project</h1>
           </Typography>
+          {!isMobile && (
+            <Paper
+              component="form"
+              sx={{
+                p: "2px 4px",
+                display: "flex",
+                alignItems: "center",
+                width: "400",
+              }}>
+              <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder="Search"
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Paper>
+          )}
+          <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
           <IconButton
             size="large"
             edge="start"
