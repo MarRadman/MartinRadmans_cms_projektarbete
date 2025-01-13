3ce2ee1ae36cadcda1e6163337fffe102353a6d1
Martin Rådmans <martin.radmans@iths.se>
2025-01-13 11:20:06 +0100

    Update: Still trying to fix the props error for vercel.

--- cms-project/src/app/projects/[slug]/page.tsx
+++ cms-project/src/app/projects/[slug]/page.tsx
@@ -14,7 +14,7 @@ import {
 import Link from "next/link";
 import styles from "./style.module.css";
 
-interface ProjectPageProps {
+interface ProjectPageProps extends ProjectData {
   params: {
     slug: string;
   };
